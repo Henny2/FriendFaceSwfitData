7ebbf06e9d28105d7c522571d79cd2f861e9301a
Henny2 <henrieke.baunack@web.de>
2023-12-25 17:46:06 -0800

    Initial Commit

---
+++ FriendFaceSwiftData/FriendFaceSwiftDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendFaceSwiftDataApp.swift
+//  FriendFaceSwiftData
+//
+//  Created by Henrieke Baunack on 12/25/23.
+//
+
+import SwiftUI
+
+@main
+struct FriendFaceSwiftDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
